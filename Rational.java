c70b44c2d990d48f0d5e50fcc1378980b8f6cb73
kzhengstuy <kzheng5tt@gmail.com>
2013-11-19 05:59:47 -0500

    Create Rational.java
    really lousy because didn't work on extensively.

---
+++ Rational.java
@@ -0,0 +1,19 @@
+public class Rational {
+    public int numerator;
+    public int denominator;
+
+    public Rational() {
+	numerator = 0;
+	denominator = 1;
+	new Rational = 0/1;
+    }
+
+    public Ratioanle(int x, int y){
+	
+    }
+
+    public String toString(int x, int y) {
+	return int x + "/" + int y;
+    }
+
+}
